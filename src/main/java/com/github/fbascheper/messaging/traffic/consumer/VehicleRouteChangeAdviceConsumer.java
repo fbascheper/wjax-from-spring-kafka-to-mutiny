f99d7e2696f213a1efa6a931e28e8f42034d4b6a
Erik-Berndt Scheper <erik.berndt.scheper@gmail.com>
2021-11-10 13:34:19 +0100

    Fixed Kafka VehicleRouteChangeAdviceConsumer

--- src/main/java/com/github/fbascheper/messaging/traffic/consumer/VehicleRouteChangeAdviceConsumer.java
+++ src/main/java/com/github/fbascheper/messaging/traffic/consumer/VehicleRouteChangeAdviceConsumer.java
@@ -1,11 +1,12 @@
 package com.github.fbascheper.messaging.traffic.consumer;
 
+import com.github.fbascheper.messaging.common.JacksonMapping;
 import com.github.fbascheper.messaging.domain.VehicleRouteChangeAdvice;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -16,22 +17,25 @@ import static org.slf4j.LoggerFactory.getLogger;
  * @author Frederieke Scheper
  * @since 20-10-2021
  */
-@Component
+@ApplicationScoped
 public class VehicleRouteChangeAdviceConsumer {
 
     private static final Logger LOGGER = getLogger(VehicleRouteChangeAdviceConsumer.class);
 
+    private final JacksonMapping jacksonMapping;
+
+    @Inject
+    public VehicleRouteChangeAdviceConsumer(JacksonMapping jacksonMapping) {
+        this.jacksonMapping = jacksonMapping;
+    }
+
     /**
-     * Handle the incoming {@link VehicleRouteChangeAdvice} and log it for demo purposes..
+     * Handle the incoming {@link VehicleRouteChangeAdvice} and log it for demo purposes.
      */
-    @KafkaListener(topics = "${traffic.kafka.vehicle-route-change-advice-topic}"
-            , clientIdPrefix = "routeChangeAdviceJson"
-            , groupId = "cgRouteAdvice"
-            , containerFactory = "kafkaListenerContainerFactory")
-    public void listenAsObject(
-            ConsumerRecord<String, VehicleRouteChangeAdvice> consumerRecord
-            , @Payload VehicleRouteChangeAdvice advice
-    ) {
+    @Incoming("vehicle-route-change-advice-kafka-csr")
+    public void consume(String adviceJson) {
+
+        var advice = jacksonMapping.fromJson(adviceJson, VehicleRouteChangeAdvice.class);
         LOGGER.debug("Received advice from Kafka => {}", advice);
     }
 
